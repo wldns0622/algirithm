61c35101fef0eb834373bbad610b70b6c682ab34
wldns0622 <wldns12378@gmail.com>
2022-03-02 01:22:05 +0900

    FIX : frequency counter challenge

--- 1_frequencyCounter/challenge.js
+++ 1_frequencyCounter/challenge.js
@@ -1,32 +1,26 @@
 // ANAGRAMS
 
 function validAnagram(str1, str2) {
-  if(str1.length !== str2.length) {
+  if (str1.length !== str2.length) {
     return false;
   }
 
-  const hashStr1 = {};
-  const hashStr2 = {};
+  const hashStr = {};
 
-  for(let value of str1) {
-    hashStr1[value] = (hashStr1[value] || 0) + 1;
-  }
-  for(let value of str2) {
-    hashStr2[value] = (hashStr2[value] || 0) + 1;
+  for (let i = 0; i < str1.length; i++) {
+    const value = str1[i];
+    hashStr[value] ? hashStr[value]++ : (hashStr[value] = 1);
   }
 
-  for(let key in hashStr1) {
-    if(!(key in hashStr2)) {
+  for (let i = 0; i < str2.length; i++) {
+    const value = str2[i];
+    if (!hashStr[value]) {
       return false;
     }
-
-    if(hashStr1[key] !== hashStr2[key]) {
-      return false;
-    }
+    hashStr[value]--;
   }
 
   return true;
 }
-
 
 module.exports = validAnagram;
